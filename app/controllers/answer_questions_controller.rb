bf71d168ec98adabc483c1be8f26bd0bab69bc67
Peter Armstrong <peter@xap.ie>
2015-07-12 19:33:06 +0100

    more awful code

--- app/controllers/answer_questions_controller.rb
+++ app/controllers/answer_questions_controller.rb
@@ -10,13 +10,11 @@ class AnswerQuestionsController < ApplicationController
 
     @requisition.questions.each do |q|
       answer = Answer.find_or_initialize_by(requisition_id: @requisition.id, 
-                    question_id: q.id)
+                                            question_id: q.id)
       answer.text = params[q.id.to_s]
       answer.save
     end
     @answers = Answer.where(requisition_id: @requisition.id).all
-      # binding.pry
-    # binding.pry
   end
 
   def set_requisition
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,4 @@
 class Answer < ActiveRecord::Base
   has_one :requisition
-  # has_one :requi_question
   has_one :question
 end
-
-
-# select * from answers where requisition.id = 1  and question.id = 1
